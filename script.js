098cea5d62fc0c66c1cd8fd20be9f3ed9e83ee62
“anisio2” <anisio2@gmail.com>
2021-05-31 15:40:05 -0300

    new comment

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
-// 1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+//1 Crie um array que receba 5 itens e exiba no console.
+let itens = ['Gato', 'Papagaio', 'Periquito', 'Tamanduá', 'Onitorrinco'];
+console.log(itens);
 
-// 2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+//2 Utilize um método para adicionar um nome ao inicio do array.
+itens.unshift('Peixe boi')
+console.log(itens)
 
-// 3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+//3 Utilize um método para remover o último nome do array.
+itens.pop();
+console.log(itens)
 
-// *** Exercícios bônus
-// 4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+//4 Utilize um método para adicionar dois nomes ao fim do array.
+itens.push('Tamanduá', 'Onitorrinco');
+console.log(itens)
 
-// 5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+//5 Utilize um método para remover o primeiro nome do array.
+var remove_ultimo = itens.shift();
+console.log(itens)
 
-let idade = 20
-let humano = "true"
-if (idade > 18){
-    console.log("Você é maior de idade")
-    console.log( "Vc é humano", humano)
-} else{
-    console.log("Rala daqui menor de idade")
-}
-
-let mes = 12 //true
-if ((mes === 12) || (mes === 01)){
-    console.log("Você FAZ aniversário em Dezembro ou Janeiro")
-} else{
-    console.log("Você não faz aniversário nesses meses")
-}
-
-let nome = "Anisio"
-if(nome.substring(0,1) === "R"){
-    console.log("Seu nome começa com R, parabéns!")
-} else{
-    console.log("Seu nome NÃO começa com R, seu nome é", nome)
-}
-
-let lastName  = "Borba"
-if ((lastName.length > 6) || (lastName.substring(0,1) === "E")){
-    console.log("Seu nome tem mais de 6 letras")
-} else{
-    console.log("Seu nome NÃO tem mais de 6 letras")
-}
+//6 Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+const numbers = [7,5,6,3,8,9,2,1,4,20];
+numbers.sort((a,b) => a-b);
+console.log(numbers)
